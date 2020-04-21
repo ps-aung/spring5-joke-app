ec6879fd5519ff928315c4b1bbdaefcfa803a27e
Pyae Sone Aung <pyaesone1511@icloud.com>
2020-04-21 12:27:45 +0630

    add spring xml configuration

--- src/main/java/psa/springframework/joke/Spring5JokeAppApplication.java
+++ src/main/java/psa/springframework/joke/Spring5JokeAppApplication.java
@@ -2,8 +2,10 @@ package psa.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5JokeAppApplication {
 
 	public static void main(String[] args) {
